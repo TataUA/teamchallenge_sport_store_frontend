0671a53bd99ff0a327a87b4ecc718c57aca3e41b
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2024-05-24 16:13:38 +0200

    added register/login forms

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,28 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import { LoginForm } from "@/app/components/LoginForm";
 
 export default function Page() {
   return (
     <>
-      <h1>Авторизація</h1>
-      <p>Увійдіть або зареєструйтесь щоб продовжити</p>
-      <Link href="/signup">Зареєструватись</Link>
+      <h1 className="mb-2">Авторизація</h1>
+      <p className="mb-6">Увійдіть або зареєструйтесь щоб продовжити</p>
+      <LoginForm />
+      <p>
+        Натискаючи на кнопку я погоджуюсь з{" "}
+        <Link href="" className="border-b-2">
+          умовами політики конфіденційності
+        </Link>{" "}
+        та
+        <Link href="" className="border-b-2">
+          {" "}
+          договором користувача
+        </Link>
+      </p>
+      <p>або</p>
+      <Link href="/signup" className="inline-block h-12 border-2">
+        Зареєструватись
+      </Link>
     </>
   );
 }
