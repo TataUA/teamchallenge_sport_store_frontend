dee89492a16aa571216b1d5eaf6738ad73832b44
Ira1234567 <iraowl1234@gmail.com>
2024-06-01 21:14:49 +0300

    Update ResetPasswordForm component

--- components/ResetPasswordForm.tsx
+++ components/ResetPasswordForm.tsx
@@ -4,8 +4,8 @@ import React from "react";
 import { useState } from "react";
 import * as yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import Link from "next/link";
 
-// #CFCFCF /
 
 export const schema = yup.object().shape({
   email: yup
@@ -28,7 +28,13 @@ const initialValues: LoginFormValues = {
 
 export const ResetPasswordForm = () => {
 
-    const [showPasswordResetBlock, setShowPasswordResetBlock] = useState<boolean>(false)
+  const [showPasswordResetBlock, setShowPasswordResetBlock] = useState<boolean>(false);
+    
+  const [popupContent, setPopupContent] = useState<string>("form");
+
+  const [labelValue, setLabelValue] = useState<string>("");
+
+  const [placeholderValue, setPlaceholderValue] = useState<string>("Електронна пошта");
 
 
   const handleInputChange = (
@@ -45,11 +51,12 @@ export const ResetPasswordForm = () => {
   ) => {
     console.log(values);
     resetForm();
+    setPopupContent("success message")
   };
 
   return (
     <>
-                <button className="block mb-5" onClick={() => {setShowPasswordResetBlock(true)}}>Забули пароль?</button>
+      <button className="block mb-5" onClick={() => {setShowPasswordResetBlock(true)}}>Забули пароль?</button>
         {showPasswordResetBlock ?
         <div className="fixed w-full h-screen top-0 left-0 flex justify-center content-center bg-blured">
         <div className="self-center bg-white p-6 rounded-popup max-w-[88vw] max-h-[88vw] text-common text-basic font-medium">
@@ -63,39 +70,48 @@ export const ResetPasswordForm = () => {
                 </p>
             </div>
             <p className="mb-4">Вкажіть свою електронну адресу і ми відправимо вам лист з інструкцією</p>
-            <Formik 
-            initialValues={initialValues}
-            validationSchema={schema}
-            onSubmit={handleSubmit}
-            >
-            {(formik) => (
-                <Form autoComplete="on">
-                <label htmlFor="email" className="block mb-4">
-                    Електронна пошта
-                    <Field
-                    id="email"
-                    type="email"
-                    name="email"
-                    placeholder="Електронна пошта"
-                    value={formik.values.email}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        handleInputChange(
-                        "email",
-                        e.target.value,
-                        formik.setFieldValue
-                        );
-                    }}
-                    className="block border-b-2 pb-2 w-full mb-8"
-                    />
-                    <ErrorMessage name="email" component="div" />
-                </label>
+            { popupContent === "form" ? 
+              <Formik 
+              initialValues={initialValues}
+              validationSchema={schema}
+              onSubmit={handleSubmit}
+              >
+              {(formik) => (
+                  <Form autoComplete="on">
+                  <label htmlFor="email" className="block mb-4">
+                      {labelValue}
+                      <Field
+                      id="email"
+                      type="email"
+                      name="email"
+                      placeholder={placeholderValue}
+                      value={formik.values.email}
+                      onFocus={() => {setLabelValue("Електронна пошта"); setPlaceholderValue("")}}
+                      onBlur={() => {setLabelValue(""); setPlaceholderValue("Електронна пошта")}}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                          handleInputChange(
+                          "email",
+                          e.target.value,
+                          formik.setFieldValue
+                          );
+                      }}
+                      className="block border-b-2 pb-2 w-full mb-8 focus:outline-none"
+                      />
+                      <ErrorMessage name="email" component="div" />
+                  </label>
 
-                <button type="submit" className="h-12 text-button bg-blue rounded-button text-white w-full font-semibold">
-                    Надіслати інструкцію
-                </button>
-                </Form>
-            )}
-            </Formik>
+                  <button type="submit" className="h-12 text-button bg-blue rounded-button text-white w-full font-semibold">
+                      Надіслати інструкцію
+                  </button>
+                  </Form>
+              )}
+              </Formik>
+              : 
+              <div>
+                <button className="my-4 h-12 text-button bg-blue rounded-button text-white w-full font-semibold mb-4" onClick={() => {setShowPasswordResetBlock(false)}}>На сторінку входу</button>
+                <button className="h-12 text-button bg-white rounded-button w-full font-semibold border-2" onClick={() => {setPopupContent("form")}}>Надіслати посилання ще раз</button>
+              </div>
+            }
         </div> 
     </div>
     : <></>
