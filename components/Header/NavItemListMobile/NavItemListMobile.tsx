f3446abd462f69f8d60475371c082d11a491539c
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-06-16 15:24:36 +0200

    Fix icon color, fix svg property names

--- components/Header/NavItemListMobile/NavItemListMobile.tsx
+++ components/Header/NavItemListMobile/NavItemListMobile.tsx
@@ -50,9 +50,9 @@ export const NavItemListMobile = ({ navItems, onClose }: NavItemListProps) => {
 						<path
 							d='M9 18L15 12L9 6'
 							stroke='#3E3E40'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
 						/>
 					</svg>
 					<div
@@ -111,9 +111,9 @@ export const NavItemListMobile = ({ navItems, onClose }: NavItemListProps) => {
 												<path
 													d='M9 18L15 12L9 6'
 													stroke='#3E3E40'
-													stroke-width='2'
-													stroke-linecap='round'
-													stroke-linejoin='round'
+													strokeWidth='2'
+													strokeLinecap='round'
+													strokeLinejoin='round'
 												/>
 											</svg>
 										</Link>
