82a306e082ceebe85327cb4ff4cbcf3ed1b63f10
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-11 18:21:40 +0300

    sh-90 created ListItem comp

---
+++ components/ProductsList/ListItem/index.tsx
@@ -0,0 +1,53 @@
+
+// utils
+import { cn } from "@/services/utils/cn"
+import getArrayWithExtractedImgUrl from "@/helpers/getArrayWithExtractedImgUrl"
+
+// components
+import { Slider } from "@/components/slider-hero/Slider"
+
+// types
+import { IProduct } from "@/services/types"
+
+const ListItem = (props: {product: IProduct}) => {
+  const {product} = props
+  const {title, price, color: colorsArray} = product
+
+  return (
+    <div className={cn(
+      'flex flex-col gap-2 pb-5',
+      "w-[32.33333%] max-[767px]:w-[48.5%] max-[767px]:pb-4 max-[380px]:w-[100%] min-[1250px]:w-[24%] min-[2800px]:gap-6"
+      )}>
+      <div className="rounded-[12px] overflow-hidden">
+        <Slider
+          productsList
+          autoPlay={false} 
+          data={getArrayWithExtractedImgUrl(product)} />
+      </div>
+      <div 
+        className="text-[#575758] truncate text-base max-[767px]:text-sm font-medium min-[2800px]:text-3xl"
+      >
+        {title}
+      </div>
+      <ul className="flex gap-2 min-[2800px]:gap-5">
+        {colorsArray?.map((color)=> (
+          <li 
+            key={color.id} 
+            className={cn(
+              `bg-${color.title.toLowerCase()} w-2 h-2 rounded-[50%] min-[2800px]:size-8`, {
+                'border': true
+              })}
+          />
+        ))}
+        <li></li>
+      </ul>
+      <div 
+        className="text-[#1A1A1C] truncate text-xl max-[767px]:text-base font-semibold min-[2800px]:text-4xl"
+      >
+        {price + ' грн'}
+      </div>
+    </div>
+  )
+}
+
+export default ListItem
