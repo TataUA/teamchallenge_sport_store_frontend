70bef5a5ffd19b1f1158abc5bccd7eb59946d464
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-11 17:03:29 +0300

    sh-90 updated fetchProductsAction

--- app/actions/fetchProductsAction.ts
+++ app/actions/fetchProductsAction.ts
@@ -4,15 +4,11 @@ import { $instance } from "@/services/api";
 import { cookies } from "next/headers";
 
 const fetchProductsAction = async () => {
-  const token = cookies().get('api_token');
-
   try {
-    $instance.defaults.headers["Authorization"] = `Bearer ${token?.value}`;
-    
     const { data } = await $instance.get("/products/");
     return data;
   } catch (error: any) {
-    throw error.response.data;
+    console.log("🚀 ~ fetchProductsAction ~ error:", error.response)
   }
 }
 
