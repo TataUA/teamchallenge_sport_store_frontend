c71020f5bbd1415ac1c491091af179e1d491370d
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-11 18:21:22 +0300

    sh-90 created List comp

---
+++ components/ProductsList/List/index.tsx
@@ -0,0 +1,55 @@
+import { useCallback } from "react"
+
+// types
+import { IProduct } from "@/services/types"
+
+// components
+import ListItem from "../ListItem"
+import Pagination from "../Pagination"
+
+interface IProps {
+  products: IProduct[]
+  page?: string
+  searchParams: {gender: string, page: string}
+}
+
+const List = (props: IProps) => {
+  const {products, searchParams} = props
+  const {gender, page} = searchParams
+
+  const currentPage = Number(page) || 1;
+
+const getPaginatedItems = useCallback((items: IProduct[], itemsPerPage: number, currentPage: number) => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const paginatedProducts = items.slice(startIndex, startIndex + itemsPerPage);
+    const countPages = items?.length / itemsPerPage
+    return {
+      paginatedProducts,
+      countPages
+    }
+  }, [])
+
+const itemsPerPage = 2;
+
+const {paginatedProducts, countPages} = getPaginatedItems(products, itemsPerPage, currentPage);
+
+  return (
+    <div>
+      <ul className="flex flex-wrap gap-2 justify-between mb-5">
+        {paginatedProducts.map((product, index) => (
+          <ListItem 
+            key={product.id} 
+            product={product} 
+          />
+          ))}
+      </ul>
+      {paginatedProducts?.length ? (
+        <Pagination
+          pageCount={countPages}
+          />
+        ) : null}
+    </div>
+  )
+}
+
+export default List
