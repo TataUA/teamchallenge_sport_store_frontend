d6615ad5b1432e6844101126b38e76f82a572c12
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-11 10:40:14 +0300

    sh-90 added types for Products response from DB

--- services/types.ts
+++ services/types.ts
@@ -28,3 +28,38 @@ export type SvgProps = {
 	fillHovered?: string
 	isHovered?: boolean
 }
+
+type GenderEnum = "Male" | "Female"
+
+interface Category {
+    id: number;
+    gender: GenderEnum;
+    sub_category: string;
+}
+
+interface Color {
+    id: number;
+    title: string;
+}
+
+interface ProductSize {
+    id: number;
+    value: string;
+}
+
+interface ProductImage {
+    id: number;
+    image_url: string;
+}
+
+export interface IProduct {
+    id: number;
+    title: string;
+    category: Category;
+    description: string;
+    price: string;
+    color: Color[];
+    size: ProductSize[];
+    images: ProductImage[];
+    quantity: string;
+}
