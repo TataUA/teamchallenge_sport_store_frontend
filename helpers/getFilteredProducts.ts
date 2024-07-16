8a6aa642e142abd89b1066c4790b9f32ad1ff5f7
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-16 15:45:33 +0300

    sh-90 created helper getFilteredProducts

---
+++ helpers/getFilteredProducts.ts
@@ -0,0 +1,26 @@
+import { IProduct } from "@/services/types";
+
+const getFilteredProducts = ({products, filters}: {products: IProduct[], filters: any[]}) => products.filter(product => 
+    filters?.every(filter => {
+        if(filter.sub_category) {
+            return product.category.sub_category.toLowerCase().includes(filter.sub_category.toLowerCase())
+        }
+        if(filter.price_to){
+            return Number(product.price) <= Number(filter.price_to)
+        }
+            
+        if(filter.price_from){
+            return Number(product.price)  >= Number(filter.price_from)
+        }
+        if(filter.color){
+            return product.color.some(c => c.title.toLowerCase().includes(filter.color.toLowerCase()))
+        }
+        if(filter.size){
+            return product.size.some(s => s.value.toLowerCase().includes(filter.size.toLowerCase()))
+        }
+            
+        return product
+    })
+);
+
+export default getFilteredProducts
