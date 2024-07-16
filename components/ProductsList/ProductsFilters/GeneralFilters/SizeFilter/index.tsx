39dc4a602b88f06c8048651f2d3cb5d235cf7dd4
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-16 21:00:03 +0300

    sh-90 updated SizeFilter

--- components/ProductsList/ProductsFilters/GeneralFilters/SizeFilter/index.tsx
+++ components/ProductsList/ProductsFilters/GeneralFilters/SizeFilter/index.tsx
@@ -1,55 +1,55 @@
 'use client'
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { cn } from "@/services/utils/cn"
+import { useDispatch, useSelector } from "react-redux";
+
+import { removeClothesSize, removeShoesSize, setClothesSize, setShoesSize } from "@/redux/generalFilters/generalFiltersSlice";
+import { selectGeneralFilters } from "@/redux/generalFilters/generalFiltersSelector";
 
 interface IProps {
-  sizes?: string[]
+  clothesSizes?: string[]
+  shoesSizes?: string[]
 }
 
 const SizeFilter = (props: IProps) => {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const currentFilterValue = searchParams.get("size") || '';
+  const dispatch = useDispatch()
+  const {shoesSizes, clothesSizes} = useSelector(selectGeneralFilters);
 
-  const createPageURLWithParams = (value: string) => {
-    const params = new URLSearchParams(searchParams);
-    const updatedValue = currentFilterValue ? currentFilterValue + ',' + value.toString() : value.toString()
-    params.set("size", updatedValue);
-
-    return `${pathname}?${params.toString()}`;
-  };
-
-  const createPageURLWithRemovedParams = (value: string) => {
-    const params = new URLSearchParams(searchParams);
-      const newValue = currentFilterValue.split(',')
-      const index = newValue.indexOf(value)
-      newValue.splice(index, 1)
-
-      params.set("size", newValue.join(','));
-    return `${pathname}?${params.toString()}`;
-  };
-
-  const handleClick = (size: string) => {
-    if(currentFilterValue.includes(size)) {
-      router.push(createPageURLWithRemovedParams(size))
+  const handleClickClothes = (size: string) => {
+    if(clothesSizes.includes(size)) {
+      dispatch(removeClothesSize(size))
       return
     }
-    router.push(createPageURLWithParams(size))
+    dispatch(setClothesSize(size))
   }
-
-  if(!props.sizes?.length) return null
+  const handleShoesClothes = (size: string) => {
+    if(shoesSizes.includes(size)) {
+      dispatch(removeShoesSize(size))
+      return
+    }
+    dispatch(setShoesSize(size))
+  }
 
   return (
     <>
       <div className="flex gap-2 flex-wrap">
-        {props.sizes?.map((size, sizeIndex) => (
+        {props.clothesSizes?.map((size, sizeIndex) => (
           <li 
-            onClick={() => handleClick(size)}
+            onClick={() => handleClickClothes(size)}
             className={cn('list-none size-12 rounded-lg border-[1px] p-4 flex justify-center items-center cursor-pointer',
               'hover:bg-blue hover:text-white', {
-                'border-[#0A4CF6] text-[#0A4CF6]': currentFilterValue.includes(size)
+                'border-[#0A4CF6] text-[#0A4CF6]': clothesSizes.includes(size)
+              }
+            )}
+            key={sizeIndex}
+          >{size}</li>
+        ))}
+        {props.shoesSizes?.map((size, sizeIndex) => (
+          <li 
+            onClick={() => handleShoesClothes(size)}
+            className={cn('list-none size-12 rounded-lg border-[1px] p-4 flex justify-center items-center cursor-pointer',
+              'hover:bg-blue hover:text-white', {
+                'border-[#0A4CF6] text-[#0A4CF6]': shoesSizes.includes(size)
               }
             )}
             key={sizeIndex}
