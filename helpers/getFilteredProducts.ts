a0cac52701f74f4f0f1abb1a994bcf9456128077
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-17 19:35:11 +0300

    sh-90 updated helper getFilteredProducts

--- helpers/getFilteredProducts.ts
+++ helpers/getFilteredProducts.ts
@@ -17,8 +17,14 @@ const getFilteredProducts = ({products, filters}: {products: IProduct[], filters
         if(filter.color){
             return product.color.some(c => c.title.toLowerCase().includes(filter.color.toLowerCase()))
         }
-        if(filter.size){
-            return product.size.some(s => s.value.toLowerCase().includes(filter.size.toLowerCase()))
+        if(filter.sizes){
+            // console.log("🚀 ~ getFilteredProducts ~ filter.sizes:", filter.sizes)
+            // const t = filter.sizes.split(',')
+            // console.log("🚀 ~ getFilteredProducts ~ t:", t)
+            // return product.size.some(s => s.value.toLowerCase().includes(filter.sizes.toLowerCase()))
+            const filterSizes = filter.sizes.split(',')?.map((s: string) => s.trim().toLowerCase());
+            const productSizes = product.size.map(s => s.value.toLowerCase());
+            return filterSizes.some((size: string) => productSizes.includes(size));
         }
             
         return product
