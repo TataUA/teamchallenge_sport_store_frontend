0f3ec29e9af561e369d375dcb02a9be460e7acd3
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-18 12:33:21 +0300

    sh-90 tets

--- app/products/[...sub_category]/page.tsx
+++ app/products/[...sub_category]/page.tsx
@@ -45,20 +45,20 @@ const getSortedAndFilteredProducts = async (filters: IFilters) => {
 	const products: IProduct[] = await fetchProductsAction()
 	console.log("🚀 ~ getSortedAndFilteredProducts ~ products:", products)
 
-  const filteredProductByCategoryAndGender = products.filter(product => 
-		(getTranslatedSubcategoryFromUkraineToEnglish(product.category.sub_category) === filters.sub_category) 
-	&& (product.category.gender.toLowerCase() === filters.gender.toLowerCase()))
-  console.log("🚀 ~ getSortedAndFilteredProducts ~ filteredProductByCategoryAndGender:", filteredProductByCategoryAndGender)
+  // const filteredProductByCategoryAndGender = products.filter(product => 
+	// 	(getTranslatedSubcategoryFromUkraineToEnglish(product.category.sub_category) === filters.sub_category) 
+	// && (product.category.gender.toLowerCase() === filters.gender.toLowerCase()))
+  // console.log("🚀 ~ getSortedAndFilteredProducts ~ filteredProductByCategoryAndGender:", filteredProductByCategoryAndGender)
 
-	const sortedProducts = getSortedProducts({products: filteredProductByCategoryAndGender, direction: filters.sortedBy})
-	console.log("🚀 ~ getSortedAndFilteredProducts ~ sortedProducts:", sortedProducts)
+	// const sortedProducts = getSortedProducts({products: filteredProductByCategoryAndGender, direction: filters.sortedBy})
+	// console.log("🚀 ~ getSortedAndFilteredProducts ~ sortedProducts:", sortedProducts)
 	
-	const arraOfFiltersFromFiltersObject = Object.entries(filters).map(([key, value]) => ({ [key]: value }));
-	console.log("🚀 ~ getSortedAndFilteredProducts ~ arraOfFiltersFromFiltersObject:", arraOfFiltersFromFiltersObject)
-	const filteredProductsByGeneralFilters = getFilteredProducts({products: sortedProducts, filters: arraOfFiltersFromFiltersObject})
-	console.log("🚀 ~ getSortedAndFilteredProducts ~ filteredProductsByGeneralFilters:", filteredProductsByGeneralFilters)
+	// const arraOfFiltersFromFiltersObject = Object.entries(filters).map(([key, value]) => ({ [key]: value }));
+	// console.log("🚀 ~ getSortedAndFilteredProducts ~ arraOfFiltersFromFiltersObject:", arraOfFiltersFromFiltersObject)
+	// const filteredProductsByGeneralFilters = getFilteredProducts({products: sortedProducts, filters: arraOfFiltersFromFiltersObject})
+	// console.log("🚀 ~ getSortedAndFilteredProducts ~ filteredProductsByGeneralFilters:", filteredProductsByGeneralFilters)
 	
-	return filteredProductsByGeneralFilters
+	return products
 };
 
 export default async function ProductsPage(props: IProductsPageInitialProps) {
