0fbbd4a739605936ff2e6d61bf07ddd4ca1d2f85
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-20 09:51:55 +0300

    sh-32 updated ColorPickerComponent

--- components/ProductById/ProductInfo/ColorPickerComponent/index.tsx
+++ components/ProductById/ProductInfo/ColorPickerComponent/index.tsx
@@ -1,19 +1,43 @@
 'use client'
 
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect, useState } from "react"
+
+// redux
 import { selectCurrentProduct } from "@/redux/currentProduct/currentProductSelector"
 import { setCurrentProductColor } from "@/redux/currentProduct/currentProductSlice"
-import { cn } from "@/services/utils/cn"
-import { useDispatch, useSelector } from "react-redux"
 
-const ColorPickerComponent = ({color}: {color: {id: number, title: string}[]}) => {
+// types
+import { IColor, IColors } from "@/services/types"
+
+// utils
+import { cn } from "@/services/utils/cn"
+import getArrayRemovedColorsDuplicates from "@/helpers/getArrayRemovedDuplicates"
+
+const ColorPickerComponent = ({colors}: {colors: IColors[]}) => {
   const dispatch = useDispatch()
+
   const {color: currentColor} = useSelector(selectCurrentProduct)
+  const [colorsWithoutDuplicates, setColorsWithoutDuplicates] = useState<IColor[]>([])
 
   const currentColorLoweCase = currentColor.toLowerCase()
 
+  
   const handleClick = (color: string) => {
-      dispatch(setCurrentProductColor(color))
+    dispatch(setCurrentProductColor(color))
   }
+  
+  useEffect(()=>{
+    setColorsWithoutDuplicates([...getArrayRemovedColorsDuplicates(colors)])
+
+  },[colors])
+
+  useEffect(()=>{
+    if(colorsWithoutDuplicates?.length) {
+      dispatch(setCurrentProductColor(colorsWithoutDuplicates[0].title.toLowerCase()))
+    }
+
+  },[dispatch, colorsWithoutDuplicates])
 
   return (
     <div className="mb-8 2xl:mb-16">
@@ -21,19 +45,19 @@ const ColorPickerComponent = ({color}: {color: {id: number, title: string}[]}) =
         Колір
         </h4>
       <ul className="flex gap-2 min-[2800px]:gap-5">
-        {color?.map((color)=> (
+        {colorsWithoutDuplicates?.map((color)=> (
           <li 
             key={color.id}
             onClick={() => handleClick(color.title)}
             className={cn(
               `min-w-[50px] min-h-[50px] rounded-[50%] min-[2800px]:size-20 relative z-20 cursor-pointer`,
               'flex items-center justify-center',
-              `${currentColorLoweCase === color.title.toLowerCase() ? `border-${currentColorLoweCase} border-[3px] border-opacity-100` : ''}`,
+              `${currentColorLoweCase === color.title.toLowerCase() ? `border-${currentColorLoweCase} border-[3px] border-opacity-900` : ''}`,
               `${'white' === color.title.toLowerCase() ? `border-gray border-[3px] border-opacity-100` : ''}`, {
               })}
           >
             <div className={cn(
-              `bg-${color.title.toLowerCase()} min-w-10 min-h-10 rounded-[50%] min-[2800px]:size-18 overflow-hidden relative z-10`, {
+              `bg-${color.title.toLowerCase()} min-w-10 min-h-10 rounded-[50%] min-[2800px]:size-18 overflow-hidden`, {
                 'border-gray border-[3px] border-opacity-100': (currentColorLoweCase === color.title.toLowerCase()) 
                   && ('white' === color.title.toLowerCase()),
               })} />
