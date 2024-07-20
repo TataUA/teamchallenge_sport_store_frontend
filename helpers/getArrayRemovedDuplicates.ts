fdabe5e4b7877538ce5e5e828b7caf324831b72e
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-20 14:06:16 +0300

    sh-32 fixed errors in console

--- helpers/getArrayRemovedDuplicates.ts
+++ helpers/getArrayRemovedDuplicates.ts
@@ -1,6 +1,6 @@
 import { IColor, IColors } from "@/services/types";
 
-  const getArrayRemovedColorsDuplicates = (array: IColors[]):IColor[]  => {
+  const getArrayRemovedColorsDuplicates = (array: IColors[] = []):IColor[]  => {
     const copiedColors = [...array].map(item => JSON.stringify(item.color))
     const set = new Set(copiedColors)
     return Array.from(set).map((item) => JSON.parse(item));
