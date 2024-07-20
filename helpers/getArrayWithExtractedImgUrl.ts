bb2d67839a58bafb8ca92fd8599227bd67ad74c9
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-20 09:53:17 +0300

    sh-32 created helper getArrayWithExtractedImgUrlWithgCurrentColor

--- helpers/getArrayWithExtractedImgUrl.ts
+++ helpers/getArrayWithExtractedImgUrl.ts
@@ -1,5 +1,9 @@
 import { IProduct } from "@/services/types"
 
-const getArrayWithExtractedImgUrl = (product: IProduct) => (product.images.map((image) => ({image: image.image_url})))
+const getArrayWithExtractedImgUrl = (product?: IProduct | null) => (product?.colors?.map(item => ({image: item.image_url})))
+
+export const getArrayWithExtractedImgUrlWithCurrentColor = (product: IProduct, currentColor: string)  => 
+  product?.colors?.filter(item => item.color.title.toLowerCase() === currentColor.toLowerCase())
+  ?.map(item => ({image: item.image_url}))
 
 export default getArrayWithExtractedImgUrl
