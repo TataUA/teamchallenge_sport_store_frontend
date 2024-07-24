e19c645d0edd36eb14f206e2e88489d17026e84a
Chmara Vlad <chmaravlad5@gmail.com>
2024-07-24 13:07:12 +0300

    sh-85 updated searchSlice and api, added searchSendQuery

--- redux/search/searchSlice.ts
+++ redux/search/searchSlice.ts
@@ -1,13 +1,32 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// api
+import { sendSearchQueryApi } from "@/services/api";
+
+// types
 import { IProduct } from "@/services/types";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface ISearchState {
-  products: IProduct[]
+  products: IProduct[] | null
+  query: string
+  loading: boolean
+  error: boolean
 }
 
 const initialState: ISearchState = {
-  products: []
+  products: null,
+  query: '',
+  loading: false,
+  error: false,
 };
+
+export const sendSearchQueryThunk = createAsyncThunk(
+  'search/sendSearchQuery',
+  async (query: string, thunkAPI) => {
+    const data = await sendSearchQueryApi(query)
+    return data
+  },
+)
 
 const searchSlice = createSlice({
   name: 'search',
@@ -16,10 +35,31 @@ const searchSlice = createSlice({
     setProductsSearchResult: (state, {payload}:  PayloadAction<IProduct[]>) => {
       state.products = [...payload]
     },
-  }
+    setSearchQuery: (state, {payload}:  PayloadAction<string>) => {
+      state.query = payload
+    },
+    setErrorNull: (state) => {
+      state.error = false
+    },
+  },
+  extraReducers: (builder) =>
+    builder
+      .addCase(sendSearchQueryThunk.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(sendSearchQueryThunk.fulfilled, (state, {payload}: PayloadAction<IProduct[]>) => {
+        state.products = [...payload]
+        state.loading = false
+      })
+      .addCase(sendSearchQueryThunk.rejected, (state) => {
+        state.loading = false
+        state.error = true
+      })
 })
 
 export const searchReducer = searchSlice.reducer;
 export const { 
   setProductsSearchResult, 
+  setSearchQuery,
+  setErrorNull
   } = searchSlice.actions;
