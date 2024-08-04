01dff77161ed58642992ccbd94d8bf35987de18a
Chmara Vlad <chmaravlad5@gmail.com>
2024-08-04 19:45:47 +0300

    sh-112 updated HeaderNavLink, added operations with cart in db logic

--- components/Header/HeaderNavLink/HeaderNavLink.tsx
+++ components/Header/HeaderNavLink/HeaderNavLink.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import Link from 'next/link'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useRef } from 'react'
 
 // data
 import { headerNav, iconsData } from '@/constants'
-
 
 // components
 import SvgComponent from '../../SvgComponent/SvgComponent'
@@ -16,9 +16,87 @@ import { cn } from '@/services/utils/cn'
 
 // store
 import { selectCart } from '@/redux/cart/cartSelector'
+import { AppDispatch } from '@/redux/store'
+import { saveCartIdFromDb, setProduct } from '@/redux/cart/cartSlice'
+
+// helpers
+import { getTokenFromLocalStorage } from '@/services/utils/get-access-token'
+
+// actions
+import createShoppingCartAction from '@/app/actions/createShoppingCartInDbAction'
+import fetchShoppingCartFromServerAction, { ICartResponseItem } from '@/app/actions/fetchShoppingCartFromServerAction'
+import fetchProductByIdClientAction from '@/app/actions/fetchProductByIdClientAction'
+
+// types
+import { IProduct } from '@/services/types'
 
 const HeaderNavLink = () => {
 	const cart = useSelector(selectCart)
+	const mounted = useRef(false)
+
+	const dispatch: AppDispatch = useDispatch()
+
+	useEffect(()  => {
+		const token = getTokenFromLocalStorage();
+
+		const fetchProductByIdAndSave = async (item: ICartResponseItem) => {
+			const productData: IProduct = await fetchProductByIdClientAction(item.product, item.color, item.size)
+
+			const colors = productData.colors.filter(colorItem => colorItem.color.id === item.color)
+			const size = productData.size.filter(sizeItem => sizeItem.id === item.size)
+			const filteredQuantities = [...productData.quantity].filter(
+				q => q.size === size[0]?.value && q.color.toLowerCase() === colors[0]?.color.title.toLowerCase()
+			)
+
+			const updatedProduct = {
+				...productData,
+				colors,
+				size,
+				quantity: [{
+					size: size[0]?.value,
+					color: colors[0]?.color.title,
+					quantity: item.quantity
+				}],
+				maxQuantity: filteredQuantities[0].quantity,
+				idInBasketInDb: item.id,
+			}
+			dispatch(setProduct(updatedProduct))
+		}
+
+		const createCartInDb = async () => {
+			// якщо створюємо нову то ми отримаємо її ІД і можемо привязати його до юзера
+			const {id} = await createShoppingCartAction()
+
+			if(id) dispatch(saveCartIdFromDb(id))
+
+			// тут можливо треба продукти с редакса зберегти в корзину в БД
+		}
+
+		const fetchCartFromDb = async (idCart: string) => {
+			const response = await fetchShoppingCartFromServerAction(idCart)
+
+			if(response?.items.length) {
+				response?.items.forEach(async (item) => {
+					fetchProductByIdAndSave(item)
+				})
+			}
+		}
+
+		// взаємодія з сервером і корзино в БД тіфльки для авторизованих юзерів
+		// для неавторизованих корзина тільки в Local Storage
+		if(true) {
+			// має бути перевірка чи має юзер корзину
+			const idCart = '462a7df0-9d55-4c58-958b-9239ad174099';
+			if(idCart) {
+				fetchCartFromDb(idCart)
+			} else {
+				// корзини немає і створюємо її
+				createCartInDb()
+			}
+		}
+
+			mounted.current = true
+	},[cart.id])
 
 	return (
 		<ul className='flex items-center'>
