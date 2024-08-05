7d7212b5d667fd8dad7dd824775526a9ce764cd6
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2024-08-05 14:37:19 +0200

    fixed access token

--- redux/auth/authSelector.ts
+++ redux/auth/authSelector.ts
@@ -1,8 +1,6 @@
 import { AuthState } from "./authSlice";
 
 export const selectUserData = (state: { auth: AuthState }) => state.auth.user;
-export const selectAccessToken = (state: { auth: AuthState }) =>
-  state.auth.accessToken;
 export const selectIsAuthenticated = (state: { auth: AuthState }) =>
   state.auth.isAuthenticated;
 export const selectIsLoading = (state: { auth: AuthState }) =>
--- services/api.ts
+++ services/api.ts
@@ -11,7 +11,6 @@ import * as types from "./types/auth-api-types";
 import { IProduct } from "./types";
 
 // helpers
-import { getTokenFromLocalStorage } from "@/services/utils/get-access-token";
 import getCorrectQueryParamsSearchQuery from "@/helpers/getCorrectQueryParamsSearchQuery";
 
 export const $instance = axios.create({
@@ -21,8 +20,9 @@ export const $instance = axios.create({
 
 $instance.interceptors.request.use(
   (config) => {
-    const token = getTokenFromLocalStorage();
-    if (token && token !== "null") {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     } else {
       config.headers["Authorization"] = "";
@@ -43,11 +43,11 @@ $instance.interceptors.response.use(
 
 //handle token
 export const setToken = (token: string) => {
-  localStorage.setItem("persist:auth", JSON.stringify({ accessToken: token }));
+  localStorage.setItem("accessToken", token);
 };
 
 export const clearToken = () => {
-  localStorage.removeItem("persist:auth");
+  localStorage.removeItem("accessToken");
 };
 
 //register user
