ec47429b9863f7c5757577c403bcd503e3f160bc
Chmara Vlad <chmaravlad5@gmail.com>
2024-09-16 18:48:54 +0300

    sh-94 created success page created order, and improved handleSubmit

---
+++ app/order/success/page.tsx
@@ -0,0 +1,11 @@
+const OrderPage = (props: any) => {
+  return (
+    <>
+      <p className="p-4 px-5 bg-[#FEEFEF] text-[#DF0707]">
+        Успішно створено ваше замовлення.
+      </p>
+    </>
+  )
+}
+
+export default OrderPage
