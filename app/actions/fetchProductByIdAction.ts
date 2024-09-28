dbc1f127bbc6c318e26dad5aa5620257043de9da
Chmara Vlad <chmaravlad5@gmail.com>
2024-09-28 15:11:14 +0300

    sh-94 updated actions, added tags for fetch

--- app/actions/fetchProductByIdAction.ts
+++ app/actions/fetchProductByIdAction.ts
@@ -1,14 +1,19 @@
 'use server';
 
 import { apiBaseUrl } from "@/services/api";
+import { revalidateTag } from "next/cache";
 
 const fetchProductByIdAction = async (id: number | string) => {
   try {
-    const result = await fetch(`${apiBaseUrl}products/${id}/`, { next: { revalidate: 3600 } });
+    const result = await fetch(`${apiBaseUrl}products/${id}/`, { next: { revalidate: 3600, tags: ['productById'] }  });
+    
     if(result.status === 200) {
       const data = result?.json()
       return data;
     }
+
+    revalidateTag('productById')
+    
     return [];
 
   } catch (error: any) {
