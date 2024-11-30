04f69e25ac1d49113a20aa98d0ebdf4d47bda43a
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2024-11-30 12:00:33 +0100

    added desktop styles on product_card screen

--- components/ProductById/ProductInfo/index.tsx
+++ components/ProductById/ProductInfo/index.tsx
@@ -1,53 +1,56 @@
-
-// componsnts
-import { Slider } from "@/components/Slider-hero/Slider"
-
 // helpers
-import getArrayWithExtractedImgUrl from "@/helpers/getArrayWithExtractedImgUrl"
-import getTranslatedSubcategoryFromUkraineToEnglish from "@/helpers/getTranslatedSubcategoryFromUkraineToEnglish"
+import getTranslatedSubcategoryFromUkraineToEnglish from "@/helpers/getTranslatedSubcategoryFromUkraineToEnglish";
 
 // components
-import SizesModal from "./SizesModal"
-import { ClientComponent } from "@/components/ClientComponent"
-import AddProductToCartComponent from "./AddProductToCartComponent"
-import ColorPickerComponent from "./ColorPickerComponent"
-import SliderComponent from "./SliderComponent"
+import SizesModal from "./SizesModal";
+import { ClientComponent } from "@/components/ClientComponent";
+import AddProductToCartComponent from "./AddProductToCartComponent";
+import ColorPickerComponent from "./ColorPickerComponent";
+import SliderComponent from "./SliderComponent";
 
 // types
-import { IProduct } from "@/services/types"
+import { IProduct } from "@/services/types";
 
-const ProductInfo = ({product}: {product: IProduct}) => {
-const {colors, price, size, quantity, category} = product
+const ProductInfo = ({ product }: { product: IProduct }) => {
+  const { colors, price, size, quantity, category } = product;
 
-const formattedDescription = product.description.replace(/\r\n/g, '<br>');
-const translatedSubCategory = getTranslatedSubcategoryFromUkraineToEnglish(category.sub_category)
+  const formattedDescription = product.description.replace(/\r\n/g, "<br>");
+  const translatedSubCategory = getTranslatedSubcategoryFromUkraineToEnglish(
+    category.sub_category,
+  );
 
   return (
-    <div>
+    <div className="1440:flex gap-5">
       <ClientComponent>
-        <SliderComponent />
-        <div>
-          <div className="text-base mb-4 lg:text-lg min-[2800px]:text-5xl min-[2800px]:mb-10">
+        <div className="1440:max-w-[628px]">
+          <SliderComponent />
+        </div>
+        <div className="1440:max-w-[628px]">
+          <div className="1440:mt-4 mb-4 1440:mb-6 text-base 1440:text-xl tracking-custom_4 1440:tracking-custom_2 text-title">
             {product.title}
           </div>
-          <div 
-            className="text-[#1A1A1C] mb-8 truncate text-2xl font-semibold lg:text-3xl lg:mb-6 min-[2800px]:text-5xl min-[2800px]:mb-10"
-          >
-            {price + ' грн'}
+          <div className="mb-8 1440:mb-10 font-semibold text-2xl tracking-custom_2 text-title">
+            {price + " грн"}
           </div>
           <ColorPickerComponent colors={colors} />
-          <SizesModal translatedSubCategory={translatedSubCategory} existedSizesFromDb={quantity} />
-          <AddProductToCartComponent product={product}/>
+          <SizesModal
+            translatedSubCategory={translatedSubCategory}
+            existedSizesFromDb={quantity}
+          />
+          <AddProductToCartComponent product={product} />
           <div>
-            <h4 className="text-base font-medium text-[#272728] lg:text-xl min-[2800px]:text-5xl min-[2800px]:mb-4">Опис</h4>
+            <h4 className="mb-2 font-semibold text-base tracking-custom_4 text-primary">
+              Опис
+            </h4>
             <div
-            className="text-sm font-medium text-[#575758] lg:text-base 2xl:text-2xl"
-            dangerouslySetInnerHTML={{ __html: formattedDescription }}></div>
+              className="font-medium text-sm tracking-custom_4 text-secondary"
+              dangerouslySetInnerHTML={{ __html: formattedDescription }}
+            ></div>
           </div>
         </div>
       </ClientComponent>
     </div>
-  )
-}
+  );
+};
 
-export default ProductInfo
+export default ProductInfo;
