b4fe5aead09308165d950500e6371fc010a8b78e
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2024-12-10 14:55:08 +0100

    fixed styles sizes modal

--- components/Shared/ResponsiveModal/index.tsx
+++ components/Shared/ResponsiveModal/index.tsx
@@ -1,39 +1,50 @@
-import { cn } from '@/services/utils/cn';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
+
+import { cn } from "@/services/utils/cn";
 
 interface IProps {
   isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
-  wrapperClassname?: string
+  wrapperClassname?: string;
 }
 
-const ResponsiveModal = ({ isOpen, onClose, children, wrapperClassname }: IProps) => {
+const ResponsiveModal = ({
+  isOpen,
+  onClose,
+  children,
+  wrapperClassname,
+}: IProps) => {
   const [isMobile, setIsMobile] = useState(false);
-  const modalRef = useRef<HTMLDivElement>(null);
   const [isDragging, setIsDragging] = useState(false);
   const [startY, setStartY] = useState(0);
   const [currentY, setCurrentY] = useState(0);
 
+  const modalRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     const checkMobile = () => setIsMobile(window.innerWidth < 768);
     checkMobile();
-    window.addEventListener('resize', checkMobile);
-    return () => window.removeEventListener('resize', checkMobile);
+    window.addEventListener("resize", checkMobile);
+
+    return () => window.removeEventListener("resize", checkMobile);
   }, []);
-  
+
   useEffect(() => {
     if (isOpen && isMobile) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     } else {
-      if(document.body.style.overflow === 'hidden') document.body.style.overflow = 'unset';
-      setCurrentY(0)
+      if (document.body.style.overflow === "hidden")
+        document.body.style.overflow = "unset";
+      setCurrentY(0);
     }
+
     return () => {
-      if(document.body.style.overflow === 'hidden') document.body.style.overflow = 'unset';
+      if (document.body.style.overflow === "hidden")
+        document.body.style.overflow = "unset";
     };
   }, [isOpen, isMobile]);
-  
+
   const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
     setIsDragging(true);
     setStartY(e.touches[0].clientY);
@@ -48,7 +59,7 @@ const ResponsiveModal = ({ isOpen, onClose, children, wrapperClassname }: IProps
       setCurrentY(diff);
     }
   };
-  
+
   const handleTouchEnd = () => {
     setIsDragging(false);
     if (currentY > window.innerHeight * 0.5) {
@@ -57,51 +68,54 @@ const ResponsiveModal = ({ isOpen, onClose, children, wrapperClassname }: IProps
       setCurrentY(0);
     }
   };
-  
-  const emptyHandler = () => {};
-  
+
   if (!isOpen) return null;
-  
+
   const modalStyle = isMobile ? { transform: `translateY(${currentY}px)` } : {};
-  
+
   return (
-    <div 
-      className={cn('fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center',
-        `${isMobile ? 'items-end' : 'items-center'}`)}
+    <div
+      className={cn(
+        "fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center",
+        `${isMobile ? "items-end" : "items-center"}`,
+      )}
       onClick={onClose}
     >
-      <div 
-      ref={modalRef}
-      className={cn('relative w-full',
-        `${isMobile ? 'max-w-full' : 'max-w-lg'}`,
-      )}
-      style={modalStyle}
-      onClick={(e) => e.stopPropagation()}
+      <div
+        ref={modalRef}
+        className={cn(
+          "relative w-full",
+          `${isMobile ? "max-w-full" : "max-w-lg"}`,
+        )}
+        style={modalStyle}
+        onClick={(e) => e.stopPropagation()}
       >
         {isMobile && (
-            <div className='h-10 p-5 absolute z-30 bottom-[100%] left-[50%] transform translate-x-[-50%]'>
-              <div 
-                className="w-14 h-1 bg-white rounded mx-auto mb-4"
-                onTouchStart={handleTouchStart}
-                onTouchMove={handleTouchMove}
-                onTouchEnd={handleTouchEnd}
-              /> 
-            </div>
-          )}
+          <div className="h-10 p-5 absolute z-30 bottom-[100%] left-[50%] transform translate-x-[-50%]">
+            <div
+              className="w-14 h-1 bg-white rounded mx-auto mb-4"
+              onTouchStart={handleTouchStart}
+              onTouchMove={handleTouchMove}
+              onTouchEnd={handleTouchEnd}
+            />
+          </div>
+        )}
         <div
-          className={cn('bg-white p-6 pt-0 w-full max-w-lg max-h-[85vh] overflow-y-auto',
-            `${isMobile ? 'max-w-full' : 'max-w-lg'}`,
-            `${isMobile 
-              ? 'rounded-t-3xl h-auto transition-transform duration-300 ease-out'
-              : 'rounded-lg'}`,
-            'min-[2800px]:p-10 min-[2800px]:rounded-3xl min-[2800px]:max-w-5xl', 
-            'md: pt-6',
-            `${wrapperClassname?.length ? wrapperClassname : ''}`,
+          className={cn(
+            "bg-white p-6 pt-0 w-full max-w-lg max-h-[85vh] overflow-y-auto",
+            `${isMobile ? "max-w-full" : "max-w-lg"}`,
+            `${
+              isMobile
+                ? "rounded-t-3xl h-auto transition-transform duration-300 ease-out"
+                : "rounded-lg"
+            }`,
+            "md: pt-6",
+            `${wrapperClassname?.length ? wrapperClassname : ""}`,
           )}
         >
           {!isMobile ? (
-            <button 
-              className="float-right text-2xl bg-transparent border-none cursor-pointer min-[2800px]:text-4xl"
+            <button
+              className="float-right text-2xl bg-transparent border-none cursor-pointer"
               onClick={onClose}
             >
               &times;
