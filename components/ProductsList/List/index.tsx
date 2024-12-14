3bd8dc9b2da611f75fdfd8d249c67c535fc9298b
OlegPichkur <oleg1pichkur@gmail.com>
2024-12-14 16:07:43 +0200

    set cards product list xl:justify-stert

--- components/ProductsList/List/index.tsx
+++ components/ProductsList/List/index.tsx
@@ -1,57 +1,60 @@
-'use client'
+"use client";
 
-import { useCallback } from "react"
+import { useCallback } from "react";
 
 // types
-import { IProduct } from "@/services/types"
+import { IProduct } from "@/services/types";
 
 // components
-import ListItem from "../ListItem"
-import Pagination from "../Pagination"
-import { IFilters } from "@/app/products/[...sub_category]/page"
+import ListItem from "../ListItem";
+import Pagination from "../Pagination";
+import { IFilters } from "@/app/products/[...sub_category]/page";
 
 interface IProps {
-  products: IProduct[]
-  searchParams: IFilters
+  products: IProduct[];
+  searchParams: IFilters;
 }
 
 const List = (props: IProps) => {
-  const {products, searchParams} = props
-  const {gender, page} = searchParams
+  const { products, searchParams } = props;
+  const { gender, page } = searchParams;
 
   const currentPage = Number(page) || 1;
 
-  const getPaginatedItems = useCallback((items: IProduct[], itemsPerPage: number, currentPage: number) => {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const paginatedProducts = items.slice(startIndex, startIndex + itemsPerPage);
-    const countPages = Math.ceil(items?.length / itemsPerPage)
-    return {
-      paginatedProducts,
-      countPages
-    }
-  }, [])
+  const getPaginatedItems = useCallback(
+    (items: IProduct[], itemsPerPage: number, currentPage: number) => {
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const paginatedProducts = items.slice(
+        startIndex,
+        startIndex + itemsPerPage,
+      );
+      const countPages = Math.ceil(items?.length / itemsPerPage);
+      return {
+        paginatedProducts,
+        countPages,
+      };
+    },
+    [],
+  );
 
-const itemsPerPage = 8;
+  const itemsPerPage = 8;
 
-const {paginatedProducts, countPages} = getPaginatedItems(products, itemsPerPage, currentPage);
+  const { paginatedProducts, countPages } = getPaginatedItems(
+    products,
+    itemsPerPage,
+    currentPage,
+  );
 
   return (
     <div>
-      <ul className="flex flex-wrap gap-2 justify-between mb-5">
+      <ul className="flex flex-wrap gap-2 justify-between xl:justify-start xl:gap-5 mb-5">
         {paginatedProducts.map((product, index) => (
-          <ListItem 
-            key={product.id} 
-            product={product}
-          />
-          ))}
+          <ListItem key={product.id} product={product} />
+        ))}
       </ul>
-      {paginatedProducts?.length ? (
-        <Pagination
-          pageCount={countPages}
-          />
-        ) : null}
+      {paginatedProducts?.length ? <Pagination pageCount={countPages} /> : null}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
