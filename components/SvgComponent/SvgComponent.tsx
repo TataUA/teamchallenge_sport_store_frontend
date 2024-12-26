e946a89e14091390a4b97e65c4def6d888108f1a
OlegPichkur <oleg1pichkur@gmail.com>
2024-12-26 17:07:54 +0200

    corrected view previous queries

--- components/SvgComponent/SvgComponent.tsx
+++ components/SvgComponent/SvgComponent.tsx
@@ -1,29 +1,29 @@
-'use client'
+"use client";
 
-import { SvgProps } from '@/services/types'
-import { cn } from '@/services/utils/cn'
+import { SvgProps } from "@/services/types";
+import { cn } from "@/services/utils/cn";
 
 const SvgComponent = ({
-	path,
-	viewBox,
-	fill = '#3E3E40',
-	fillHovered = '#868687',
-	isHovered,
-	stroke,
-	strokeHovered,
-	classname = '',
+  path,
+  viewBox,
+  fill = "#3E3E40",
+  fillHovered = "#868687",
+  isHovered,
+  stroke,
+  strokeHovered,
+  classname = "",
 }: SvgProps) => {
-	return (
-		<svg
-			className={cn('w-6 h-6', classname)}
-			viewBox={viewBox}
-			fill={!isHovered ? fill : fillHovered}
-			stroke={!isHovered ? stroke : strokeHovered}
-			xmlns='http://www.w3.org/2000/svg'
-		>
-			<path d={path} />
-		</svg>
-	)
-}
+  return (
+    <svg
+      className={cn("w-6 h-6", classname)}
+      viewBox={viewBox}
+      fill={!isHovered ? fill : fillHovered}
+      stroke={!isHovered ? stroke : strokeHovered}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d={path} />
+    </svg>
+  );
+};
 
-export default SvgComponent
+export default SvgComponent;
