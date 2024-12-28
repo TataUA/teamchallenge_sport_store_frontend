1b21388a1af8fc7b6e2d40439421a53a6dbbdcbe
Chmara Vlad <chmaravlad5@gmail.com>
2024-12-28 12:15:18 +0200

    created new action bindShopingCart

---
+++ app/actions/bindShopinCartToUser.ts
@@ -0,0 +1,31 @@
+"use client";
+
+import { $instance } from "@/services/api";
+
+const bindShopingCartToUser = async (basketId: string) => {
+  try {
+    const result = await $instance.post(
+      "baskets/bind/",
+      { basket_id: basketId },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    if (result.status !== 201) {
+      throw new Error(JSON.stringify(result.data));
+    }
+
+    const { data }: { data: { basket_id: string; user_id: number[] } } = result;
+
+    return data.basket_id;
+  } catch (error: any) {
+    console.log("🚀 ~ bindShopingCartToUser ~ error:", error.response);
+
+    return null;
+  }
+};
+
+export default bindShopingCartToUser;
