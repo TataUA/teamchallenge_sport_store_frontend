f2fbd153a7d7b53a955a279e6da376b2939589d2
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2024-12-30 13:26:25 +0200

    update import

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,12 +1,13 @@
-import { ClientComponent } from "@/components/ClientComponent"
-import OrderPageComponent from "@/components/OrderPageComponent"
-import SubscribeBannerFooter from "@/components/SubscribeBannerFooter"
+import { ClientComponent } from "@/components/ClientComponent";
+import OrderPageComponent from "@/components/OrderPageComponent";
+import { SubscribeBannerFooter } from "@/components/SubscribeBannerFooter";
 
 const OrderPage = (props: any) => {
   return (
     <>
       <p className="p-4 px-5 bg-[#FEEFEF] text-[#DF0707]">
-        Увага! Цей пет-проєкт створений виключно для навчальних цілей. Будь ласка, не вводьте свої реальні особитсі данні
+        Увага! Цей пет-проєкт створений виключно для навчальних цілей. Будь
+        ласка, не вводьте свої реальні особитсі данні
       </p>
       <div className="p-6 pt-0">
         <ClientComponent>
@@ -15,7 +16,7 @@ const OrderPage = (props: any) => {
       </div>
       <SubscribeBannerFooter />
     </>
-  )
-}
+  );
+};
 
-export default OrderPage
+export default OrderPage;
