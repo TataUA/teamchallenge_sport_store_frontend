89b8d10d6b219e9904a2c6afdb4db3854598e17d
Chmara Vlad <chmaravlad5@gmail.com>
2025-01-06 20:18:34 +0200

    created new layout ProfileLayout

---
+++ app/ProfileLayout.tsx
@@ -0,0 +1,23 @@
+// components
+import { PrivateRouteComponent } from "@/components/Auth/PrivateRouterComponent";
+import { ClientComponent } from "@/components/ClientComponent";
+import ProfileMenu from "@/components/ProfileMenu";
+import { SubscribeBannerFooter } from "@/components/SubscribeBannerFooter";
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function ProfileLayout({ children }: RootLayoutProps) {
+  return (
+    <ClientComponent>
+      <PrivateRouteComponent>
+        <div className="container pb-10 flex-col md:flex md:flex-row flex-1 gap-5 lg:gap-[75px] lg:mt-12 md:pt-9">
+          <ProfileMenu />
+          <div className="flex-1 w-full flex flex-col">{children}</div>
+        </div>
+        <SubscribeBannerFooter />
+      </PrivateRouteComponent>
+    </ClientComponent>
+  );
+}
