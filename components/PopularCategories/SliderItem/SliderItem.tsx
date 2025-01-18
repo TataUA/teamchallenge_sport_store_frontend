27176c1c712415d069d4715c666431d3f83b6b78
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2025-01-18 15:33:13 +0100

    fixed slider popular categories

--- components/PopularCategories/SliderItem/SliderItem.tsx
+++ components/PopularCategories/SliderItem/SliderItem.tsx
@@ -1,30 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { cn } from "@/services/utils/cn";
 import { PopularCategories } from "@/public/data/popular-categories.data";
 
 export const SliderItem = ({ item }: { item: PopularCategories }) => {
   return (
-    <div className="">
+    <div className="w-full h-[156px] xl:h-[180px] flex justify-center items-center">
       <Link
-        href={item.href ?? "/"}
-        className="flex flex-col gap-2 w-full h-full cursor-pointer"
+        href={item.href}
+        className="flex flex-col justify-start gap-2 w-full h-full cursor-pointer"
       >
-        <div className="relative flex flex-col items-center justify-center w-full min-h-[108px] xl:min-h-[181px] max-h-[181px] rounded-xl overflow-hidden">
+        <div className="relative w-full h-[108px] xl:h-[180px] overflow-hidden rounded-xl">
           <Image
-            src={item.image || ""}
+            src={item.image}
             alt={item.title}
-            width={312}
-            height={181}
-            className="w-full h-full object-cover"
+            fill
+            priority
+            className="object-cover object-center"
           />
-          <div className="absolute left-0 top-0 w-full h-full z-5 bg-gradient-to-t from-black/20 to-black/0 rounded-xl">
-            {" "}
-          </div>
+          <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-black/0"></div>
         </div>
-
-        <h3 className="flex items-center text-center h-10 xl:h-7 xl:absolute z-10 left-6 bottom-6 font-medium text-sm xl:text-xl tracking-custom_2 text-title xl:text-white">
+        <h3 className="text-left w-[100px] md:w-full h-10 xl:h-7 xl:absolute z-10 left-6 bottom-6 font-medium text-sm xl:text-xl tracking-custom_2 text-title xl:text-white">
           {item.title}
         </h3>
       </Link>
--- components/PopularCategories/index.tsx
+++ components/PopularCategories/index.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-import { v4 as uuidv4 } from "uuid";
-
 import { SliderWrapper } from "@/components/PopularCategories/SliderWrapper/SliderWrapper";
 import { popularCategories } from "@/public/data/popular-categories.data";
 
@@ -11,37 +7,7 @@ export const PopularCategories = () => {
       <h2 className="mb-4 xl:mb-8 font-semibold text-xl xl:text-2xl text-title">
         Популярні категорії
       </h2>
-      {/* <ul className="flex flex-wrap gap-2 xl:gap-6">
-        {popularCategories.map(({ href, image, title }) => (
-          <li
-            key={uuidv4()}
-            className="relative w-[calc((100%-8px)/2)] xl:w-[312px] "
-          >
-            <Link
-              href={href ?? "/"}
-              className="flex flex-col gap-2 w-full h-full cursor-pointer"
-            >
-              <div className="relative flex flex-col items-center justify-center w-full min-h-[108px] xl:min-h-[181px] max-h-[181px] rounded-xl overflow-hidden">
-                <Image
-                  src={image || ""}
-                  alt={title}
-                  width={312}
-                  height={181}
-                  className="w-full h-full object-cover"
-                />
-                <div className="absolute left-0 top-0 w-full h-full z-5     bg-gradient-to-t from-black/20 to-black/0 rounded-xl">
-                  {" "}
-                </div>
-              </div>
-
-              <h3 className="flex items-center text-center h-10 xl:h-7 xl:absolute z-10 left-6 bottom-6 font-medium text-sm xl:text-xl tracking-custom_2 text-title xl:text-white">
-                {title}
-              </h3>
-            </Link>
-          </li>
-        ))}
-      </ul> */}
-      <SliderWrapper popular={ popularCategories} />
+      <SliderWrapper popular={popularCategories} />
     </section>
   );
 };
