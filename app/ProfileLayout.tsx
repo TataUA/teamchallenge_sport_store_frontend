8738d32f8b2111c085913f95417acfceafbf5d65
OlegPichkur <oleg1pichkur@gmail.com>
2025-01-20 13:11:24 +0200

    set top space for mobile into auth/...

--- app/ProfileLayout.tsx
+++ app/ProfileLayout.tsx
@@ -11,7 +11,7 @@ export default function ProfileLayout({ children }: RootLayoutProps) {
   return (
     <ClientComponent>
       <PrivateRouteComponent>
-        <div className="container h-[640px] mb-10 flex gap-5 lg:gap-5">
+        <div className="container h-[640px] mb-10 flex gap-5 lg:gap-5 mt-14 xl:mt-[-8px]">
           <ProfileMenu />
           <div className="flex-1 w-full flex flex-col">{children}</div>
         </div>
