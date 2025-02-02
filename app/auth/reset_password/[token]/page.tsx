8d4d6eb5f984baddb723023a0d219a5a6af7aa08
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2025-02-02 12:13:12 +0100

    fixed shadows, border-raunded in comfirmed letter/change password

--- app/auth/reset_password/[token]/page.tsx
+++ app/auth/reset_password/[token]/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <ClientComponent>
       <div className="container 1440:h-full 1440:flex items-center justify-center">
-        <div className="w-full 1440:w-[504px] 1440:my-4 1440:p-10 1440:border 1440:border-border 1440:rounded-lg 1440:shadow-lg">
+        <div className="w-full 1440:w-[504px] 1440:my-4 1440:p-10 1440:border 1440:border-border 1440:rounded-3xl 1440:shadow-custom">
           <h1 className="mt-4 1440:mt-0 mb-2 text-2xl leading-140 font-pangram font-bold text-title">
             Відновлення паролю
           </h1>
